938b7babefae65b1f118f610982b68cae911b7ef
AlexandreLeFou <alehunter@hotmail.gr>
2018-11-29 23:35:56 +0200

    refactored view of Vehicles.java

--- src/Vehicles.java
+++ src/Vehicles.java
@@ -10,30 +10,13 @@ public class Vehicles  {
         this.ownerName = ownerName;
         this.plateNumber = plateNumber;
         this.carType = carType;
-        this.finishDayInsu = finishDayInsu;
-    }
+        this.finishDayInsu = finishDayInsu;    }
 
 
-    public String getPlateNumber() {
-        return plateNumber;
-    }
+    public String getPlateNumber() { return plateNumber;  }
 
-    /* public int Compare(Vehicles d, Vehicles d1) {
-         return d.getPlateNumber().compareTo(d1.getPlateNumber());
-     }
- */
-    public String getOwnerName() {
-        return ownerName;
-    }
+    public String getOwnerName()   { return ownerName;    }
 
-    public void setOwnerName(String ownerName) {
-        this.ownerName = ownerName;
-    }
+    public void setOwnerName(String ownerName) { this.ownerName = ownerName;   }
 
-    public String getFinishDayInsu() {
-        return finishDayInsu;
-    }
-
-
-
-}
+    public String getFinishDayInsu() {  return finishDayInsu; } }
